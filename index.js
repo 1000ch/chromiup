625376b20d778735f36b36d961364c23cf6104a6
1000ch <shogo.sensui@gmail.com>
2016-10-26 03:18:59 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const fs = require('fs');
 const pify = require('pify');
 const trash = require('trash');
 const download = require('download');
-const fsP = pify(fs);
+const fs = pify(require('fs'));
 
 const url = 'https://download-chromium.appspot.com/dl/Mac?type=snapshots';
 const src = '/tmp/chrome-mac/Chromium.app';
@@ -10,4 +9,4 @@ const dest = '/Applications/Chromium.app';
 
 module.exports = () => trash([src, dest])
   .then(() => download(url, '/tmp', { extract: true }))
-  .then(() => fsP.rename(src, dest));
+  .then(() => fs.rename(src, dest));
